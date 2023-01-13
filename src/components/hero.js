d662a9d071eaccf3fdb6c1c0ce86f93a99be47ae
Bryan Tempini <mistatempini@gmail.com>
2023-01-13 11:36:29 -0500

    testing more images

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section id="hero">
       <h2 id="design">Web Design</h2>
-      <img src="AND.png" alt="decorative" id="and" />
+      <img src="../images/AND.png" alt="decorative" id="and" />
       <h2 id="development">Development</h2>
     </section>
   );
--- src/components/icons.js
+++ src/components/icons.js
@@ -3,21 +3,17 @@ import React from "react";
 const Icons = () => {
   return (
     <div id="icons">
-      <img class="languageIcon" src="html-5 1.png" alt="html5 icon" />
-      <img class="languageIcon" src="css 1.png" alt="css icon" />
+      <img class="languageIcon" src="../images/html-5 1.png" alt="html5 icon" />
+      <img class="languageIcon" src="../images/css 1.png" alt="css icon" />
       <img
         class="languageIcon"
-        src="../../public/java-script 1.png"
+        src="../images/java-script 1.png"
         alt="javascript icon"
       />
-      <img class="languageIcon" src="node.png" alt="node icon" />
-      <img
-        class="languageIcon"
-        src="../../public/atom 1.png"
-        alt="react icon"
-      />
-      <img class="languageIcon" src="sql.png" alt="sql icon" />
-      <img class="languageIcon" src="mongodb.png" alt="mongo icon" />
+      <img class="languageIcon" src="../images/node.png" alt="node icon" />
+      <img class="languageIcon" src="../images/atom 1.png" alt="react icon" />
+      <img class="languageIcon" src="../images/sql.png" alt="sql icon" />
+      <img class="languageIcon" src="../images/mongodb.png" alt="mongo icon" />
     </div>
   );
 };
